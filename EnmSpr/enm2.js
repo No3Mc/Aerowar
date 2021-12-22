4b7085d40719cb139243c1869d4643ebbeb4896b
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-22 14:43:24 +0000

    EnmSpr/enm2:

--- EnmSpr/enm2.js
+++ EnmSpr/enm2.js
@@ -1,1 +1,95 @@
-﻿
+﻿//enemy 1
+function enemy2() {
+    //initialisation code will go here
+
+    //create private variables for the x and y coordinates
+    var x = 750,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
+        y = 200, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
+        vx = 0,
+        vy = 0;
+
+    //create the draw function to give us the draw method
+    //it accepts one parameter which is the context from the canvas it is drawn on
+    enemy2.prototype.draw = function (context) {
+        //save the state of the drawing context before we change it
+        context.save();
+        //set the coordinates of the drawing area of the new shape to x and y
+        context.translate(x, y);
+        //start the line (path)
+        
+        context.beginPath();
+
+
+         //Starting point
+        context.moveTo(0, 40);
+
+        context.fillStyle = '#FFA500';
+//2
+        context.lineTo(-15, 25);
+        context.lineTo(-15, 20);
+        context.lineTo(-18, 20)
+
+        context.lineTo(-50, 5)
+
+//3
+        context.lineTo(-60, -10)
+        context.lineTo(-40, -5)
+
+        context.lineTo(-15, -12)
+        context.lineTo(-15, -15)
+        context.lineTo(-25, -22)
+        context.lineTo(-25, -26)
+        context.lineTo(-23, -26)
+        context.lineTo(-20, -24)
+        context.lineTo(-20, -25)
+        context.lineTo(-15, -25)
+        context.lineTo(-15, -24)
+        context.lineTo(-12, -24)
+        context.lineTo(-12, -25)
+        context.lineTo(-10, -25)
+        context.lineTo(-10, -26)
+        
+//4
+        context.lineTo(0, -30)
+        context.lineTo(10, -26)
+        context.lineTo(10, -25)
+        context.lineTo(12, -25)
+        context.lineTo(12, -24)
+        context.lineTo(15, -24)
+        context.lineTo(15, -25)
+        context.lineTo(20, -25)
+        context.lineTo(20, -24)
+        context.lineTo(23, -26)
+        context.lineTo(25, -26)
+        context.lineTo(25, -22)
+        context.lineTo(15, -15)
+        context.lineTo(15, -12)
+
+        context.lineTo(40, -5)
+        context.lineTo(60, -10)
+
+//1
+        context.lineTo(50, 5)
+        context.lineTo(18, 20)
+        context.lineTo(15, 20);
+        context.lineTo(15, 25);
+
+        context.lineTo(0, 40);
+
+
+
+        //close the path
+        context.closePath();
+        context.fill();
+        //go ahead and draw the line
+        context.stroke();
+
+
+        //restore the state of the context to what it was before our drawing
+        context.restore();
+    }
+
+
+}
+
+
