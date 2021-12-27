7c66161b5a086977b586d3b1ef792bad293738d4
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-27 19:57:24 +0000

    Classes/boundary:

--- Classes/boundary.js
+++ Classes/boundary.js
@@ -4,37 +4,25 @@ function boundz() {
     //save the state of the drawing context before we change it
     context.save();
     //set the coordinates of the drawing area of the new shape to x and y
-    context.translate(x, y);
+    // context.translate(x, y);
     //start the line (path)
         
     context.beginPath();
 
 
          //Starting point
-    context.moveTo(0, -40);
+    // context.moveTo(0, -40);
 
-    context.fillStyle = 'rgb(0, 0, 0)';
-    context.moveTo(0, -40);
+    // context.fillStyle = 'rgb(0, 0, 0)';
+    // context.moveTo(0, -40);
 
-    context.fillStyle = 'rgb(0, 0, 0)';
-
-    context.lineTo(50, 300);
-    context.moveTo(300, 100);
-
-    context.lineTo(50, 5)
-    context.lineTo(15, 25);
-
-    context.lineTo(60, 10)
-    context.lineTo(15, 5);
-
-    context.lineTo(60, 10)
-    context.lineTo(15, 25);
-
-    context.lineTo(15, 25);
-    context.lineTo(15, 25);
-
+    // context.fillStyle = 'rgb(0, 0, 0)';
+    context.moveTo(0,0)
+    context.lineTo(0, 900);
+    context.lineTo(2040, 900);
+    context.lineTo(2040, 0);
     context.closePath();
-    context.fill();
+    context.stroke();
     //go ahead and draw the line
     context.stroke();
     //restore the state of the context to what it was before our drawing
