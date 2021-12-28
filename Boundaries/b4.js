d770cb0a04e1b1b2c8d4931512f0f2e4cd83bc15
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-28 20:53:05 +0000

    Boundaries/b4:

---
+++ Boundaries/b4.js
@@ -0,0 +1,134 @@
+
+//create the constructor for the class saucer
+function bound4() {
+    //initialisation code will go here
+
+    //create private variables for the x and y coordinates
+    var x = 600,
+        y = 1250,
+        vx = 0,
+        vy = 0,
+        Boom = false;
+
+
+    //create the draw function to give us the draw method
+    //it accepts one parameter which is the context from the canvas it is drawn on
+    bound4.prototype.draw = function (context) {
+        //save the state of the drawing context before we change it
+        context.save();
+        //set the coordinates of the drawing area of the new shape to x and y
+        context.translate(x, y);
+        //start the line (path)
+        context.beginPath();
+        
+        context.lineTo(-600, -400);
+        context.lineTo(1440, -400);
+
+
+        //close the path
+        context.closePath();
+        context.fill();
+        //go ahead and draw the line
+        context.stroke();
+
+
+
+        //if the ship has blown up
+        if (Boom == true) {
+            //create a new instance of an image
+            var img = new Image();
+            //get the bitmap source
+            img.src = "boom.png";
+            //draw the image on the context
+            context.drawImage(img, -100, -60);
+        }
+ 
+
+
+        //restore the state of the context to what it was before our drawing
+        context.restore();
+    }
+
+
+
+    bound4.prototype.move = function ()
+    {
+        //change the x axis by the x velocity
+        x += vx;
+        //change the y axis by the y velocity
+        y += vy;
+    }
+
+    bound4.prototype.setVector = function(vector)
+    {
+        //set the vx value based on this vector
+        vx = vector.VX;
+        //set the vy value based on this vector
+        vy = vector.VY;
+    }
+
+    //public method to set the vector of the saucer
+    bound4.prototype.accelerate = function (Acceleration) {
+        //set vx
+        vx += Acceleration.AX;
+        //set vy
+        vy += Acceleration.AY;
+    }
+
+    //create a public property called Top
+    Object.defineProperty(this, 'Top',
+    {
+        //getter
+        get: function () {
+            //return the value of y less height
+            return y - 20;
+        }
+    }
+    )
+
+    //create a public property called Bottom
+    Object.defineProperty(this, 'Bottom',
+    {
+        //getter
+        get: function () {
+            //return the value of y plus height
+            return y + 20;
+        }
+    }
+    )
+
+    //create a public property called Left
+    Object.defineProperty(this, 'Left',
+    {
+        //getter
+        get: function () {
+            //return the value of x less width
+            return x - 30;
+        }
+    }
+    )
+
+    //create a public property called Right
+    Object.defineProperty(this, 'Right',
+    {
+        //getter
+        get: function () {
+            //return the value of x plus width
+            return x + 30;
+        }
+    }
+    )
+
+    bound4.prototype.halt = function ()
+    {
+        //temp variable to store the vy
+        var temp = vy;
+        //kill all velocity
+        vx = 0;
+        vy = 0;
+        //set the ship as exploding
+        if (temp > .4) {
+            Boom = true;
+        }
+    }
+}
