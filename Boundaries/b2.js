4037cec266a2da7c092a46c0fd9fb1684f1db325
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-28 15:33:54 +0000

    Boundaries/b2:

--- Boundaries/b2.js
+++ Boundaries/b2.js
@@ -1,5 +1,5 @@
 function bound2() {
-
+    var Boom = false;
     bound2.prototype.draw = function (context) {
     //save the state of the drawing context before we change it
     context.save();
@@ -16,9 +16,10 @@ function bound2() {
     // context.fillStyle = 'rgb(0, 0, 0)';
     // context.moveTo(0, -40);
 
-    // context.fillStyle = 'rgb(0, 0, 0)';
-    context.moveTo(0,900)
-    context.lineTo(2040, 900);
+    
+    context.moveTo(0,0)
+    context.lineTo(0, 900);
+
 
     context.closePath();
     context.stroke();
@@ -28,6 +29,17 @@ function bound2() {
     context.restore();
 
     }
-
+    bound2.prototype.halt = function ()
+    {
+        //temp variable to store the vy
+        var temp = vy;
+        //kill all velocity
+        vx = 0;
+        vy = 0;
+        //set the ship as exploding
+        if (temp > .4) {
+            Boom = true;
+        }
+    }
 
 }
