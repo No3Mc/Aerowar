5dd6446fde53c4e5796eb7a639b5009fead6508c
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-28 20:58:10 +0000

    MnSprites/spMain:

--- MnSprites/spMain.js
+++ MnSprites/spMain.js
@@ -5,7 +5,7 @@ function Mainspr() {
 
     //create private variables for the x and y coordinates
     var x = 950,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
-        y = 750, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
+        y = 800, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
         vx = 0,
         vy = 0,
         Boom = false;
