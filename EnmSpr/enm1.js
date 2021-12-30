f1338b4205073ff27c698f5b337556b23a076ebb
No3Mc <41834061+No3Mc@users.noreply.github.com>
2021-12-30 00:37:18 +0000

    EnmSpr/enm1:

--- EnmSpr/enm1.js
+++ EnmSpr/enm1.js
@@ -100,10 +100,10 @@ function enemy() {
         //initialisation code will go here
     
         //create private variables for the x and y coordinates
-        this.x = Math.random() * 400;
-        this.y = Math.random() * 200;
+        this.x = Math.random() * -1800;
+        this.y = Math.random() * 400;
         //set the size of the star
-        var size = 10;
+        var size = 70;
     
         enemy.prototype.draw = function (context) {
             //save the state of the drawing context before we change it
@@ -113,11 +113,70 @@ function enemy() {
             context.translate(this.x, this.y);
             //start the line (path)
             context.beginPath();
-            context.moveTo(-size, size);
-            context.lineTo(size, size);
-            context.lineTo(size, -size);
-            context.lineTo(size, -size);
-            context.lineTo(-size, -size);
+        //     context.moveTo(-size, size);
+        //     context.lineTo(size, size);
+        //     context.lineTo(size, -size);
+        //     context.lineTo(size, -size);
+        //     context.lineTo(-size, -size);
+
+
+
+        context.moveTo(0, 40);
+
+        context.fillStyle = '#FFA500';
+//2
+        context.lineTo(-15, 25);
+        context.lineTo(-15, 20);
+        context.lineTo(-18, 20)
+
+        context.lineTo(-50, 5)
+
+//3
+        context.lineTo(-60, -10)
+        context.lineTo(-40, -5)
+
+        context.lineTo(-15, -12)
+        context.lineTo(-15, -15)
+        context.lineTo(-25, -22)
+        context.lineTo(-25, -26)
+        context.lineTo(-23, -26)
+        context.lineTo(-20, -24)
+        context.lineTo(-20, -25)
+        context.lineTo(-15, -25)
+        context.lineTo(-15, -24)
+        context.lineTo(-12, -24)
+        context.lineTo(-12, -25)
+        context.lineTo(-10, -25)
+        context.lineTo(-10, -26)
+        
+//4
+        context.lineTo(0, -30)
+        context.lineTo(10, -26)
+        context.lineTo(10, -25)
+        context.lineTo(12, -25)
+        context.lineTo(12, -24)
+        context.lineTo(15, -24)
+        context.lineTo(15, -25)
+        context.lineTo(20, -25)
+        context.lineTo(20, -24)
+        context.lineTo(23, -26)
+        context.lineTo(25, -26)
+        context.lineTo(25, -22)
+        context.lineTo(15, -15)
+        context.lineTo(15, -12)
+
+        context.lineTo(40, -5)
+        context.lineTo(60, -10)
+
+//1
+        context.lineTo(50, 5)
+        context.lineTo(18, 20)
+        context.lineTo(15, 20);
+        context.lineTo(15, 25);
+
+        context.lineTo(0, 40);
+
+
             //context.lineTo(0, 0);
             //close the path
             context.closePath();
