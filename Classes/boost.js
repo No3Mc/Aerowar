c6a6d9be644fee6514d75157d1a7ae8fca181cff
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-01-01 09:24:36 +0000

    Classes/boost:

--- Classes/boost.js
+++ Classes/boost.js
@@ -3,8 +3,8 @@ function Boost(){
     var x = 950,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
         y = 800, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
         vx = 0,
-        vy = 0,
-
+        vy = 0;
+        
     //create the draw function to give us the draw method
     //it accepts one parameter which is the context from the canvas it is drawn on
     Boost.prototype.draw = function (context) {
@@ -26,11 +26,11 @@ function Boost(){
         context.lineTo(0, 30)
         context.lineTo(5, 40)
         context.lineTo(20, 50);
-        context.lineTo(5, 26);
 
 
 
 
+        
         context.closePath();
         context.fill();
         //go ahead and draw the line
@@ -39,3 +39,5 @@ function Boost(){
         //restore the state of the context to what it was before our drawing
         context.restore();
 }
+
+
