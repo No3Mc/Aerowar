e6cfa6563e87be3893dd31ce288e3f0d6b85d67f
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-01-03 08:49:03 +0000

    MnSprites/bullets:

--- MnSprites/bullets.js
+++ MnSprites/bullets.js
@@ -4,7 +4,7 @@ function bullets() {
     //initialisation code will go here
     //create private variables for the x and y coordinates
     var x = 950,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
-        y = 770, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
+        y = 670, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
         vx = 0,
         vy = 0,
         GlobeYellow = 1;
