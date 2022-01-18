9206e7b537a55dac8c0adb5361dc9cc6cd7cb54e
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-01-18 23:03:23 +0000

    Classes/boost:

--- Classes/boost.js
+++ Classes/boost.js
@@ -1,10 +1,13 @@
-function Boost(){
+function Boost(x, y){
 
-    var x = 950,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
-        y = 800, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
-        vx = 0,
-        vy = 0;
-        
+    // var x = 950,//(left right) smaller the number, the closer to left bounds, and the larger, more closer to right bounds
+    //     y = 800, //(up down) smaller the number, the closer to bottom bounds, and the larger, more closer to upper bounds
+    //     vx = 0,
+    //     vy = 0;
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
     //create the draw function to give us the draw method
     //it accepts one parameter which is the context from the canvas it is drawn on
     Boost.prototype.draw = function (context) {
