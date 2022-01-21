cb0b00fab7c4ad792cd58bbfafb556802138e2cb
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-01-21 02:29:21 +0000

    EnmSpr/enm3:

--- EnmSpr/enm3.js
+++ EnmSpr/enm3.js
@@ -3,10 +3,10 @@ function enemy3() {
     //initialisation code will go here
 
     dis3x = 110;
-    dis3y = 400;
+    dis3y = 200;
     enm3Color = '#39177A';
 
-    enem3speed = 4;
+    enem3speed = 3;
     // for collision
     topEdge = 10
     bottemEdge = 10, 
